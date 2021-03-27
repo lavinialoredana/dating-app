e2f44592d2e044814ef23a29e69e52b4a978f046
Lavinia Loredana <lavinialoreda@gmail.com>
2021-03-27 18:05:02 +0100

    created the ChatPage component

--- src/ChatPage.js
+++ src/ChatPage.js
@@ -1,2 +1,22 @@
+import React from "react";
+import Message from "./Message"
+import myAvatar from './images/user1.png';
+import userAvatar from './images/user2.png';
+
+
+
+
+const ChatPage = () => {
+  
+return (
+<div className="chat-box-container">
+ <div className="chat-box">
    <Message img = {userAvatar} text ="Lorem ipsum scripta manet"/> 
    <Message img = {myAvatar} text ="Lorem ipsum smanent scripta?"/> 
+ </div>
+ <input className="new-message" type="text" />
+</div>
+)
+}
+
+export default ChatPage;
