78c8eb14849c513cf33e21b2c2fa4aff26cd6523
Lavinia Loredana <lavinialoreda@gmail.com>
2021-03-27 21:50:23 +0100

    user profile styles

--- src/Profile.js
+++ src/Profile.js
@@ -1,42 +1,48 @@
 import React from "react";
 
-
 import Button from "./Button";
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import avatar from './images/user1.png';
-import {  Link } from "react-router-dom";
-
-
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import avatar from "./images/user1.jpg";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
-        // I choose to declare the user details constants inside the function to avoid repetition of this information throughout the code
+  // I choose to declare the user details constants inside the function to avoid repetition of this information throughout the code
   const userName = "Sophie";
   const userAge = 22;
   const userLocation = "New York";
-  const userDescription = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error nihil porro accusantium totam dolorum consectetur tempore repellat id debitis placeat qui dolorem delectus iusto, nisi quibusdam quasi architecto. Deserunt, facilis";
+  const userDescription =
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error nihil porro accusantium totam dolorum consectetur tempore repellat id debitis placeat.";
   return (
-    <div>
-      <Link to="/chatpage">
-        <img src={avatar} alt="profile" />
-        </Link>
-
-      <div className= "profile-info">       
+    <div className="profile-container">
+      <div> 
+        <div className="profile-online-user">
+          <Link to="/chatpage">
+            <img src={avatar} alt="profile-photo" className="circle-img" />
+          </Link>
+          
+          <div className="online-status">
+            online
+          </div>
+        </div>
+        
+      <div className="profile-details">
         <div>
-          <h1>{userName}, {userAge}</h1>
-          <FontAwesomeIcon icon={faStar}/>
+          <h2>
+            {userName}, {userAge}
+          </h2>
+          <span> {userLocation}</span>
         </div>
 
-        <span> {userLocation}</span>
-        <p>{userDescription}</p>
-         
-         <Button content="ADD AS FRIEND"/> 
+        <FontAwesomeIcon icon={faStar} />
       </div>
 
+        <p>{userDescription}</p>
+        </div>
+      <Button content="ADD AS FRIEND" />
     </div>
-
-
+    
   );
 };
 
-export default Profile
+export default Profile;
