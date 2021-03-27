ca04175da110bdc3dc3c62f1c4e0fb71cac48b03
Lavinia Loredana <lavinialoreda@gmail.com>
2021-03-27 18:09:08 +0100

    implemented the React router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 import ChatPage from "./ChatPage";
 import Header from "./Header";
 import Profile from "./Profile";
 import { BrowserRouter, Route } from "react-router-dom";
 
+
+
+
 function App() {
   return (
+    <BrowserRouter> 
     <Header/>
+    <Route exact path="/"> 
     <Profile/>
+    </Route>
+    <Route exact path="/chatpage"> 
       <ChatPage/>
+    </Route>
+   
+    
+    
+    </BrowserRouter>
   );
 }
 
