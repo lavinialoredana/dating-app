8ae5d2c8d9ee81292d92321915150bbf30e0a91b
Lavinia Loredana <lavinialoreda@gmail.com>
2021-03-27 18:08:05 +0100

    adding the import files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import ChatPage from "./ChatPage";
+import Header from "./Header";
+import Profile from "./Profile";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
