be1635fd0c99e0b3826dcdeba469c3e95b5b4d53
Lavinia Loredana <lavinialoreda@gmail.com>
2021-03-28 14:47:02 +0200

    Finished the Chat Page styles

--- src/ChatPage.js
+++ src/ChatPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Message from "./Message"
-import myAvatar from './images/user1.jpg';
-import userAvatar from './images/user2.jpg';
+import userAvatar from './images/user1.jpg';
+import myAvatar from './images/user2.jpg';
 
 
 
@@ -11,10 +11,12 @@ const ChatPage = () => {
 return (
 <div className="chat-box-container">
  <div className="chat-box">
-   <Message img = {userAvatar} text ="Lorem ipsum scripta manet"/> 
-   <Message img = {myAvatar} text ="Lorem ipsum smanent scripta?"/> 
+   <Message img = {userAvatar} text ="Lorem ipsum scripta manet vorba volent scripta manet n'es pas?" user = "guest"  /> 
+   <Message img = {myAvatar} text ="Lorem ipsum smanent scripta?" user = "self" /> 
  </div>
- <input className="new-message" type="text" />
+ <div className="new-message-wrapper">  
+ <input className="new-message" type="text" placeholder="Write a message..." />
+</div>
 </div>
 )
 }
